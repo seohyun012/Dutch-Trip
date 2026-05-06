700cee1693f85df957ab30ba98576d09ba142543
jiyoung0531 <jiyoung05317@gmail.com>
2026-05-06 16:17:31 +0900

    feat:페이지 연결 및 마이페이지 생성

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Mypage() {
+    return (
+        <main className="bg-white min-h-screen w-full flex flex-col items-center justify-center gap-12 p-5 overflow-hidden">
+            <div className="flex flex-col items-center gap-4">
+        <h1 className="text-4xl font-black text-black">마이페이지</h1>
+        <p className="text-black/80 text-lg">여행기록 확인</p>
+      </div>
+        </main>
+    );
+}
